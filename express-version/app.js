71083d58348cf159e088a33cb7161991fe4a2d07
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-04 16:59:34 +0400

    feature: list added

--- express-version/app.js
+++ express-version/app.js
@@ -5,13 +5,18 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res)=>{
-    res.render('index', {message: 'Shushanik rocks backend'})
+const links = [
+    { href: '/', text: 'Home' },
+    { href: '/about', text: 'About' }
+];
+
+app.get('/', (req, res) => {
+    res.render('index', { message: 'Shushanik rocks backend!', links: links })
 });
 
 let port = process.env.PORT || 3000
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    
+
 });
